151816ee479dbaba10162e516589209e1821c0ef
juancrfig <juancrfig@gmail.com>
2025-03-27 11:45:24 -0500

    Add keymap for do a fuzzy finder in home dir

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,8 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+-- Mapping to search from the home directory using Telescope
+vim.keymap.set('n', '<leader>fh', function()
+  builtin.find_files({ cwd = vim.fn.expand("~") })
+end, { desc = "Telescope: Find files in home directory" })
